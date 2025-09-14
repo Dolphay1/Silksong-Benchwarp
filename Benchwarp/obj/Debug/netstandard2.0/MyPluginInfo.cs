c927009e477339db236b5dfe037bd144a460afbd
Ryan Thornton <104332783+Dolphay1@users.noreply.github.com>
2025-09-14 12:49:26 -0400

    Delete Benchwarp/obj directory

--- Benchwarp/obj/Debug/netstandard2.0/MyPluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace SamplePlugin
-{
-    public static class MyPluginInfo
-    {
-        public const string PLUGIN_GUID = "SamplePlugin";
-        public const string PLUGIN_NAME = "My first plugin";
-        public const string PLUGIN_VERSION = "1.0.0";
-    }
-}
--- Benchwarp/obj/Debug/netstandard2.1/MyPluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace Benchwarp
-{
-    public static class MyPluginInfo
-    {
-        public const string PLUGIN_GUID = "Benchwarp";
-        public const string PLUGIN_NAME = "org.dolphay.Benchwarp";
-        public const string PLUGIN_VERSION = "1.0.0";
-    }
-}
